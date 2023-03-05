6b695a23cb79e5ef88697078750a35381af4d0f0
elee1109 <elee2@fandm.edu>
2023-03-05 16:29:51 -0500

    tutorial added when starting app for first time

--- app/src/main/java/net/fandm/eli_lenin/wordly/MainActivity.java
+++ app/src/main/java/net/fandm/eli_lenin/wordly/MainActivity.java
@@ -1,8 +1,13 @@
 package net.fandm.eli_lenin.wordly;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**
+         * ChatGPT3 derived code for flagging first time app launch
+         */
+        // Get a reference to the shared preferences object
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        // Check if the "firstTime" flag is set
+        if (sharedPreferences.getBoolean("firstTime", true)) {
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("firstTime", false);
+            editor.apply();
+            TutorialFragment tutorialFragment = new TutorialFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.add(R.id.fragment_container, tutorialFragment);
+            transaction.commit();
+
+
+            // Do something for the first time, like show an introduction or tutorial screen
+        } else {
+            Toast.makeText(this, "Welcome Back!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
---
+++ app/src/main/java/net/fandm/eli_lenin/wordly/TutorialFragment.java
@@ -0,0 +1,16 @@
+package net.fandm.eli_lenin.wordly;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TutorialFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tutorial, container, false);
+    }
+}
