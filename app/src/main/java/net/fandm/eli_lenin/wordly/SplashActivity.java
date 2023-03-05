b02b9905a44d43831a8a636f5f9bc728c735963a
Lennin Antunish <lantunish285@gmail.com>
2023-03-05 15:41:21 -0500

    empty activity for splash screen

---
+++ app/src/main/java/net/fandm/eli_lenin/wordly/SplashActivity.java
@@ -0,0 +1,14 @@
+package net.fandm.eli_lenin.wordly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
