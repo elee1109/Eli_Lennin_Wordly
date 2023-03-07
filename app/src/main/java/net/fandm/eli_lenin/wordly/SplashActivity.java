ed4ec0ece13bb6185e78d1abba2284dced5c0103
Lennin Antunish <lantunish285@gmail.com>
2023-03-07 06:18:47 -0500

    splash screen updated to remove nav bar

--- app/src/main/java/net/fandm/eli_lenin/wordly/SplashActivity.java
+++ app/src/main/java/net/fandm/eli_lenin/wordly/SplashActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private  View mDecorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
+        mDecorView = getWindow().getDecorView();
+        mDecorView.setOnSystemUiVisibilityChangeListener(
+                new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            hideSystemUI();
+                        }
+                    }
+                });
+
+        hideSystemUI();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -27,5 +40,22 @@ public class SplashActivity extends AppCompatActivity {
         }, 3000); // 3000 is the time in milliseconds
     }
 
+    private void hideSystemUI() {
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            hideSystemUI();
+        }
+        return true;
+    }
 
 }
