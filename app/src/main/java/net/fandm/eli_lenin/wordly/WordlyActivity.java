d73dbc40867cf5281eaff07f30516cb6be0930d3
Lennin Antunish <lantunish285@gmail.com>
2023-03-08 19:29:14 -0500

    added immersive sticky to UI

--- app/src/main/java/net/fandm/eli_lenin/wordly/WordlyActivity.java
+++ app/src/main/java/net/fandm/eli_lenin/wordly/WordlyActivity.java
@@ -287,7 +287,7 @@ public class WordlyActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility( View.SYSTEM_UI_FLAG_IMMERSIVE |
+        decorView.setSystemUiVisibility( View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
@@ -297,7 +297,7 @@ public class WordlyActivity extends AppCompatActivity {
 
     private void hideSystemUI() {
         decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
